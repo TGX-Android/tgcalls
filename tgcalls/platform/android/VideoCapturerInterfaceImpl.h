b95e75f589d6c987bbb6dfbebf8ea42d1eeb5b4d
vkryl <6242627+vkryl@users.noreply.github.com>
2023-03-30 01:11:28 +0400

    Fixed NDK build error caused by missing `VideoCapturerInterfaceImpl` functon declarations

--- tgcalls/platform/android/VideoCapturerInterfaceImpl.h
+++ tgcalls/platform/android/VideoCapturerInterfaceImpl.h
@@ -10,14 +10,12 @@ namespace tgcalls {
 class VideoCapturerInterfaceImpl final : public VideoCapturerInterface {
 public:
 	VideoCapturerInterfaceImpl(rtc::scoped_refptr<webrtc::JavaVideoTrackSourceInterface> source, std::string deviceId, std::function<void(VideoState)> stateUpdated, std::shared_ptr<PlatformContext> platformContext);
-  ~VideoCapturerInterfaceImpl() override;
 
 	void setState(VideoState state) override;
 	void setPreferredCaptureAspectRatio(float aspectRatio) override;
 	void setUncroppedOutput(std::shared_ptr<rtc::VideoSinkInterface<webrtc::VideoFrame>> sink) override;
-  int getRotation() override {
-		return 0;
-	}
+  int getRotation() override;
+  void setOnFatalError(std::function<void()> error) override;
 
 private:
 	std::unique_ptr<VideoCameraCapturer> _capturer;
